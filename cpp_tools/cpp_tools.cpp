cf3fc7b4dc871f01c05c7a0ec69a1f1554f52748
roodahpeach <roodahpeach@gmail.com>
2021-09-23 09:01:21 +0900

    fix: 예외처리

--- cpp_tools/cpp_tools.cpp
+++ cpp_tools/cpp_tools.cpp
@@ -2,114 +2,123 @@
 
 bool cpp_tools::Log_Start(string log_file_name)
 {
-    cpp_tools::log_file_name = log_file_name + "_" + cpp_tools::Date_GetDateNTime_ForFileName() + ".csv";
-    cpp_tools::logfile_ofstream.open(cpp_tools::log_file_name); //Log_½Ã°£.csv ÀúÀå
-    bool result = logfile_ofstream.fail();
-    return !result;
+	cpp_tools::log_file_name = log_file_name + "_" + cpp_tools::Date_GetDateNTime_ForFileName() + ".csv";
+	cpp_tools::logfile_ofstream.open(cpp_tools::log_file_name); //Log_½Ã°£.csv ÀúÀå
+	bool result = logfile_ofstream.fail();
+	return !result;
 }
 
 bool cpp_tools::Log_Add(string str)
 {
-    this->log_string.append(str);
-    this->log_string.append(",");
-    return true;
+	if (cpp_tools::logfile_ofstream.good()) {
+		this->log_string.append(str);
+		this->log_string.append(",");
+		return true;
+	}
+	return false;
 }
 
 bool cpp_tools::Log_Endline()
 {
-    this->log_string.pop_back(); // ½°Ç¥ ¶¼±â
-    cpp_tools::logfile_ofstream << this->log_string << endl;
-    this->log_string.clear();
-    return true;
+	if (cpp_tools::logfile_ofstream.good()) {
+		this->log_string.pop_back(); // ½°Ç¥ ¶¼±â
+		cpp_tools::logfile_ofstream << this->log_string << endl;
+		this->log_string.clear();
+		return true;
+	}
+	return false;
 }
 
 bool cpp_tools::Log_end()
 {
-    cpp_tools::logfile_ofstream.close();
-    return true;
+	if (this->logfile_ofstream.is_open()) {
+		cpp_tools::logfile_ofstream.close();
+		return true;
+	}
+	return false;
 }
 
 
 
 string cpp_tools::Date_GetDateNTime()
 {
-    struct tm curr_tm;
-    time_t curr_time = time(nullptr);
+	struct tm curr_tm;
+	time_t curr_time = time(nullptr);
 
-    localtime_s(&curr_tm, &curr_time);
+	localtime_s(&curr_tm, &curr_time);
 
-    int curr_month = curr_tm.tm_mon + 1;
-    int curr_day = curr_tm.tm_mday;
-    int curr_hour = curr_tm.tm_hour;
-    int curr_minute = curr_tm.tm_min;
-    int curr_second = curr_tm.tm_sec;
+	int curr_month = curr_tm.tm_mon + 1;
+	int curr_day = curr_tm.tm_mday;
+	int curr_hour = curr_tm.tm_hour;
+	int curr_minute = curr_tm.tm_min;
+	int curr_second = curr_tm.tm_sec;
 
-    string out_str;
+	string out_str;
 
-    out_str += to_string(curr_month);
-    out_str += "/";
-    out_str += to_string(curr_day);
-    out_str += " - ";
-    out_str += to_string(curr_hour);
-    out_str += ":";
-    out_str += to_string(curr_minute);
-    out_str += ":";
-    out_str += to_string(curr_second);
+	out_str += to_string(curr_month);
+	out_str += "/";
+	out_str += to_string(curr_day);
+	out_str += " - ";
+	out_str += to_string(curr_hour);
+	out_str += ":";
+	out_str += to_string(curr_minute);
+	out_str += ":";
+	out_str += to_string(curr_second);
 
-    return out_str;
+	return out_str;
 }
 
 string cpp_tools::Date_GetDateNTime_ForFileName()
 {
-    struct tm curr_tm;
-    time_t curr_time = time(nullptr);
+	struct tm curr_tm;
+	time_t curr_time = time(nullptr);
 
-    localtime_s(&curr_tm, &curr_time);
+	localtime_s(&curr_tm, &curr_time);
 
-    int curr_month = curr_tm.tm_mon + 1;
-    int curr_day = curr_tm.tm_mday;
-    int curr_hour = curr_tm.tm_hour;
-    int curr_minute = curr_tm.tm_min;
-    int curr_second = curr_tm.tm_sec;
+	int curr_month = curr_tm.tm_mon + 1;
+	int curr_day = curr_tm.tm_mday;
+	int curr_hour = curr_tm.tm_hour;
+	int curr_minute = curr_tm.tm_min;
+	int curr_second = curr_tm.tm_sec;
 
-    string out_str;
+	string out_str;
 
-    out_str += to_string(curr_month);
-    out_str += "¿ù";
-    out_str += to_string(curr_day);
-    out_str += "ÀÏ-";
-    out_str += to_string(curr_hour);
-    out_str += "½Ã";
-    out_str += to_string(curr_minute);
-    out_str += "ºÐ";
-    out_str += to_string(curr_second);
-    out_str += "ÃÊ";
+	out_str += to_string(curr_month);
+	out_str += "¿ù";
+	out_str += to_string(curr_day);
+	out_str += "ÀÏ-";
+	out_str += to_string(curr_hour);
+	out_str += "½Ã";
+	out_str += to_string(curr_minute);
+	out_str += "ºÐ";
+	out_str += to_string(curr_second);
+	out_str += "ÃÊ";
 
-    return out_str;
+	return out_str;
 
-    return string();
+	return string();
 }
 
 string cpp_tools::Date_GetTime()
 {
-    struct tm curr_tm;
-    time_t curr_time = time(nullptr);
+	struct tm curr_tm;
+	time_t curr_time = time(nullptr);
 
-    localtime_s(&curr_tm, &curr_time);
+	localtime_s(&curr_tm, &curr_time);
 
-    int curr_hour = curr_tm.tm_hour;
-    int curr_minute = curr_tm.tm_min;
-    int curr_second = curr_tm.tm_sec;
+	int curr_hour = curr_tm.tm_hour;
+	int curr_minute = curr_tm.tm_min;
+	int curr_second = curr_tm.tm_sec;
 
-    string out_str;
+	string out_str;
 
-    out_str += to_string(curr_hour);
-    out_str += ":";
-    out_str += to_string(curr_minute);
-    out_str += ":";
-    out_str += to_string(curr_second);
+	out_str += to_string(curr_hour);
+	out_str += ":";
+	out_str += to_string(curr_minute);
+	out_str += ":";
+	out_str += to_string(curr_second);
 
-    return out_str;
+	return out_str;
 }
 
 //=====================================================================================================
@@ -117,65 +126,65 @@ string cpp_tools::Date_GetTime()
 
 bool cpp_tools::Stopwatch_GetQPF(__int64* QPFTicksPerSec)
 {
-    LARGE_INTEGER li;
+	LARGE_INTEGER li;
 
-    if (QueryPerformanceFrequency(&li) == false)
-    {
-        return false;
-    }
+	if (QueryPerformanceFrequency(&li) == false)
+	{
+		return false;
+	}
 
-    *QPFTicksPerSec = static_cast<__int64>(li.QuadPart);
+	*QPFTicksPerSec = static_cast<__int64>(li.QuadPart);
 
-    return true;
+	return true;
 }
 
 __int64 cpp_tools::Stopwatch_GetQPCTick(void)
 {
-    LARGE_INTEGER li;
+	LARGE_INTEGER li;
 
-    QueryPerformanceCounter(&li);
+	QueryPerformanceCounter(&li);
 
-    return static_cast<__int64>(li.QuadPart);
+	return static_cast<__int64>(li.QuadPart);
 }
 
 bool cpp_tools::Stopwatch_Start()
 {
-    this->_Stopwatch_Length = 0;
-    return this->Stopwatch_GetQPF(&this->TicksPerSec);
+	this->_Stopwatch_Length = 0;
+	return this->Stopwatch_GetQPF(&this->TicksPerSec);
 }
 
 bool cpp_tools::Stopwatch_End()
 {
-    this->_Stopwatch_Length = 0;
-    this->TicksPerSec = 0;
-    return true;
+	this->_Stopwatch_Length = 0;
+	this->TicksPerSec = 0;
+	return true;
 }
 
 int cpp_tools::Stopwatch_Length()
 {
-    return this->_Stopwatch_Length;
+	return this->_Stopwatch_Length;
 }
 
 bool cpp_tools::Stopwatch_Check_Startpoint(int checkpoint_index)
 {
-    this->Tick_Array_Start[checkpoint_index] = cpp_tools::Stopwatch_GetQPCTick();
-    return true;
+	this->Tick_Array_Start[checkpoint_index] = cpp_tools::Stopwatch_GetQPCTick();
+	return true;
 }
 
 bool cpp_tools::Stopwatch_Check_Endpoint(int checkpoint_index) {
-    this->_Stopwatch_Length++;
-    this->Tick_Array_End[checkpoint_index] = cpp_tools::Stopwatch_GetQPCTick();
-    return true;
+	this->_Stopwatch_Length++;
+	this->Tick_Array_End[checkpoint_index] = cpp_tools::Stopwatch_GetQPCTick();
+	return true;
 }
 
 float cpp_tools::Stopwatch_GetTime_s(int checkpoint_index)
 {
-    return ((float)(Tick_Array_End[checkpoint_index] - Tick_Array_Start[checkpoint_index]) / this->TicksPerSec);
+	return ((float)(Tick_Array_End[checkpoint_index] - Tick_Array_Start[checkpoint_index]) / this->TicksPerSec);
 }
 
 float cpp_tools::Stopwatch_GetTime_ms(int checkpoint_index)
 {
-    return ((float)(Tick_Array_End[checkpoint_index] - Tick_Array_Start[checkpoint_index]) / this->TicksPerSec) * 1000;
+	return ((float)(Tick_Array_End[checkpoint_index] - Tick_Array_Start[checkpoint_index]) / this->TicksPerSec) * 1000;
 }
 
 //=====================================================================================================
