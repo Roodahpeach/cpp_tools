36ecfff61d512411709bfa38e9ae0e69f68800c0
KKM <kkm@comizoa.com>
2022-06-27 13:11:08 +0900

    chore: INI 기능 예제 저장

--- cpp_tools/cpp_tools.h
+++ cpp_tools/cpp_tools.h
@@ -14,6 +14,34 @@
 
 using namespace std;
 
+CString INIReadString(CString strAppName, CString strKeyName, CString strFilePath)
+{
+	char szReturnString[1024] = { 0, };
+	memset(szReturnString, NULL, 1024);
+	GetPrivateProfileString(strAppName, strKeyName, "", szReturnString, 1024, strFilePath);
+	CString str;
+	str.Format("%s", szReturnString);
+	return str;
+}
+
+void INIWriteString(CString strAppName, CString strKeyName, CString strValue, CString strFilePath)
+{
+	WritePrivateProfileString(strAppName, strKeyName, strValue, strFilePath);
+}
+
+CString GetExePath()
+{
+	g_criticalExe.Lock();
+	static TCHAR pBuf[256] = { 0, };
+	memset(pBuf, NULL, sizeof(pBuf));
+	GetModuleFileName(NULL, pBuf, sizeof(pBuf)); //현재 실행 경로를 가져오는 함수
+	CString strFilePath;
+	strFilePath.Format(_T("%s"), pBuf);
+	strFilePath = strFilePath.Left(strFilePath.ReverseFind(_T('\\')));
+	g_criticalExe.Unlock();
+	return strFilePath;
+}
+
 class cpp_tools {
 private:
 #pragma region Private_Variable
